fb0c0d3ffd5ea3e0d9d90ee94fca7488170a53e3
Tanesaka <seedtanesa@outlook.jp>
2020-11-20 16:08:18 +0000

    Update

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd004a0d5755ba6b0b38b362bccb67e467759bbafd0dc93a18c8b369cbdeb7ec61f91956c737dfdf7c79ebd5a61dd6170fc1a66cdc43d7a5fa11aae6825db579b'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/show'
+  devise_for :users
+  root to: 'homes#top'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+# 親のresourcesで指定したコントローラ名に、子のresourcesで指定したコントローラ名が続くURLが生成されるのが確認できます。
+# このような親子関係を、「ネストする」と言います。
+# 必要ではないけど、「ある投稿画像に関連付けられたコメント」と認識できるURLのほうが、ユーザにとってわかりやすいです。
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
 end
